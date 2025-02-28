79866bf657a9c7ff6eb12d83efe7d32c4fc62334
Romaha135 <polisukroma705@gmail.com>
2025-02-28 13:21:11 +0200

    Refactor views to use template rendering and add HTML templates for index and about pages (Lesson 4)

--- main/views.py
+++ main/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse # This line is not needed
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<h4>Hello, world. You're at the polls index.</h4>")
+    return render(request, 'main/index.html')
 
 def about(request):
-    return HttpResponse("<h4>This is the about page.</h4>")
+    return render(request, 'main/about.html')
