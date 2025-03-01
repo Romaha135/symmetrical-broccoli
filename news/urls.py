e675af7a7ea692358ba2e2ae05d60c35a594fa4b
Romaha135 <polisukroma705@gmail.com>
2025-03-01 23:38:11 +0200

    Add news app with news_home view, URL patterns, and template (Lesson 7 part 1)

---
+++ news/urls.py
@@ -0,0 +1,10 @@
+# Description: This file contains the URL patterns for the news app.
+from django.urls import path
+from . import views
+
+
+
+urlpatterns = [
+    path('', views.news_home, name='news_home'),
+    
+]
