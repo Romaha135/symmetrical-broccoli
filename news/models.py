1228d8debef953bc446a14fce08923d6a348d1df
Romaha135 <polisukroma705@gmail.com>
2025-03-02 17:02:51 +0200

    Add Articles model with fields and register in admin (Lesson 7 part 2)

--- news/models.py
+++ news/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Articles(models.Model):
+    title = models.CharField('Title', max_length=50, default='Untitled')
+    intro = models.CharField('Introduction', max_length=250, default='No introduction')
+    full_text = models.TextField('Article', default='No content')  # Add a default value
+    date = models.DateTimeField('Date')
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name = 'Article'
+        verbose_name_plural = 'Articles'
+
